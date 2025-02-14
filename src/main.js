3aa3bbe6980fcacf25c23d89fcf25e8bdb53b952
mugesz <kannamugesh135@gmail.com>
2025-02-14 14:52:16 +0530

    3d added

--- src/main.js
+++ src/main.js
@@ -14,43 +14,35 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
 
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 // Create a torus
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 ); 
+const material = new THREE.MeshStandardMaterial( { color: 0xff6347, wireframe:true} ); 
+const torus = new THREE.Mesh( geometry, material );
+ scene.add( torus );
 
-const length = 12, width = 8;
+// Create an ambient light for base illumination
+const ambientLight = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add(ambientLight);
 
-const shape = new THREE.Shape();
-shape.moveTo( 0,0 );
-shape.lineTo( 0, width );
-shape.lineTo( length, width );
-shape.lineTo( length, 0 );
-shape.lineTo( 0, 0 );
+// Create a point light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(20, 20, 20);
+scene.add(pointLight);
 
-const extrudeSettings = {
-	steps: 2,
-	depth: 16,
-	bevelEnabled: true,
-	bevelThickness: 1,
-	bevelSize: 1,
-	bevelOffset: 0,
-	bevelSegments: 1
-};
 
-const geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-const mesh = new THREE.Mesh( geometry, material ) ;
-scene.add( mesh );
 
 // Animation function
 function animate() {
   requestAnimationFrame(animate);
 
-  mesh.rotation.x += 0.005;
-  mesh.rotation.y += 0.02;
-  mesh.rotation.z += 0.005;
+  torus.rotation.x += 0.005;
+  torus.rotation.y += 0.02;
+  torus.rotation.z += 0.005;
   renderer.render(scene, camera);
 }
 
